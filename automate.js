d18de28db18f1ec8d9a57006db7950fedc5c0665
Sam Kauffman <samvnkauffman@gmail.com>
2018-10-27 22:13:35 -0400

    WIP::automation

--- automate.js
+++ automate.js
@@ -14,51 +14,80 @@ function getDayOfTheWeek(date) {
 
 function completeTimesheet(config) {
     config = { blacklist: [], ...config }; // defaults
-    function fillOutTimesheet() {
-        function continueLoop() {
-            $('.input-group-addon').click();
+
+    function nextDay() {
+        $('.input-group-addon').click();
+        setTimeout(() => {
+            const $nextDayNode = $('.active.day').nextElementSibling || $('.active.day').parentElement.nextElementSibling.firstElementChild;
+            $nextDayNode.click();
+
             setTimeout(() => {
-                const $nextDayNode = $('.active.day').nextElementSibling || $('.active.day').parentElement.nextElementSibling.firstElementChild;
-                $nextDayNode.click();
-                setTimeout(fillOutTimesheet, 0);
-            }, 100);
+                const date = dateSlashesToDashes($('#date1').value);
+                if (config.blacklist.includes(date)) {
+                    nextDay();
+                }
+            }, 200); // delay for click
 
-        }
+        }, 200); // delay for click
 
-        const date = new Date( dateSlashesToDashes($('#date1').value) );
 
-        const blackListDates = config.blacklist.map((blacklistDate) => +new Date(blacklistDate));
-        if ( blackListDates.includes(+date) ) {
-            continueLoop();
-        }
-
-        const dayOfTheWeek = getDayOfTheWeek(date);
-
-        const activities = Object.keys(config.days[dayOfTheWeek] || {});
-
-        activities.forEach((activityKey) => {
-            activity = config.activity[activityKey];
-
-            if (activity.capitalize) {
-                $('#yes-select-button').click();
-            }
-            if (activity.international) {
-                $('#yes-int-select-button').click();
-            }
-            $('#time-units > [value="h"]').selected = true; // always choose hours
-            $('#time').value = config.days[dayOfTheWeek][activityKey];
-            $('#details').value = activity.details;
-
-            // $('#activity-button').click();
-        });
-
-        if (+date < +new Date(config.endDate)) {
-            continueLoop();
-        }
     }
 
-    // location.assign(`/version2/lab/hourglass/home.html?day=${config.startDate}`); // FIXME::method needs to run even after page change
-    fillOutTimesheet(config);
+    const date = new Date( dateSlashesToDashes($('#date1').value) );
+
+    const { activity } = queryString();
+
+    if (+date > +new Date(config.endDate)) {
+        console.log(`End date (${config.endDate}) reached.`);
+        return;
+    }
+
+    const dayOfTheWeek = getDayOfTheWeek(date);
+
+    const activities = Object.keys(config.days[dayOfTheWeek] || {});
+
+    // if no activities for the day, continue
+    if (!activities.length) {
+        nextDay();
+    }
+
+    if (!activity) {
+        location.assign(`${location.pathname}?day=${config.startDate}&activity=${activities[0]}`);
+        return;
+    }
+
+    // const activityKey = activity || Object.keys(config.days[dayOfTheWeek] || {})[0];
+    //
+    // if (!activityKey) {
+    //     nextDay();
+    // }
+
+    filloutTimesheet(activityKey, dayOfTheWeek);
+
+    function filloutTimesheet(activityKey, dayOfTheWeek) {
+        activity = config.activity[activityKey];
+
+        if (activity.capitalize) {
+            $('#yes-select-button').click();
+        }
+        if (activity.international) {
+            $('#yes-int-select-button').click();
+        }
+        $('#time-units > [value="h"]').selected = true; // always choose hours
+        $('#time').value = config.days[dayOfTheWeek][activityKey];
+        $('#details').value = activity.details;
+
+        $('#activity-button').click();
+    }
+
+    // if final activity, continue
+
+    activities.forEach((activityKey) => {
+    });
+
+    if (+date < +new Date(config.endDate)) {
+        continueLoop();
+    }
 }
 
 const config = {
@@ -86,4 +115,22 @@ const config = {
     },
 }
 
-completeTimesheet(config);
+// START
+function start(config) {
+    if (config.startDate >= config.endDate) {
+        console.error('End Date must be later than Start Date');
+        return;
+    }
+
+    const { start, day } = queryString();
+
+    if (start) {
+        if (day === config.startDate) {
+            completeTimesheet(config);
+        } else {
+            location.assign(`${location.pathname}?day=${config.startDate}&start=1`);
+        }
+    }
+}
+
+start(config);
--- utils.js
+++ utils.js
@@ -1,2 +1,14 @@
 const $ = (...args) => document.querySelector(...args);
 const $All = (...args) => document.querySelectorAll(...args);
+
+function queryString(str = location.search) {
+    return str
+        .slice(1)
+        .split('&')
+        .map(p => p.split('='))
+        .reduce((obj, pair) => {
+            let [key, value] = pair.map(decodeURIComponent);
+            value = isNaN(value) ? value : +value;
+            return ({ ...obj, [key]: value })
+        }, {});
+}
